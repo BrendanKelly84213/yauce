22247f01bd6fdca391b5117414a251d83c15cd67
Brendan Kelly <brewkelly@gmail.com>
2021-12-02 16:21:59 -0800

    Generator: Added some useful functions to the header for debugging in other files

--- src/Generator.h
+++ src/Generator.h
@@ -9,6 +9,13 @@ typedef uint64_t Bitboard;
 typedef uint16_t BMove;
 
 std::vector<Move> boardstate_to_move_vec(BoardState board_state);
+void init_generator();
+Bitboard get_piece_moves(PieceType p, Square from, Bitboard occ);
+void print(Bitboard bb); 
+PieceType piece_to_piecetype(Piece piece); 
+Bitboard occ_squares(Piece* squares, Colour colour);
+Bitboard get_behind(Square from, Square to);
+inline Square pop_bit(Bitboard &bb);
 
 
 #endif
