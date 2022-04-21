40f50855bdabd2e7908e408552ebf4daa7618ee4
Brendan Kelly <brewkelly@gmail.com>
2022-04-21 12:47:28 -0700

    implement in_check_generator in perft for test

--- src/test/perft.cpp
+++ src/test/perft.cpp
@@ -3,14 +3,19 @@
 int perft(int depth, BoardState board_state, Stats &stats)
 {
    BMove moves[256];
-   BoardState copy = board_state;
+   /* BoardState copy = board_state; */
    int nodes = 0;
    int num_moves = 0;
 
    if(depth == 0)
        return 1ULL;
 
-   num_moves = psuedo_generator(board_state, moves);
+   Colour us = board_state.get_side_to_move();
+   if(board_state.in_check(us)) {
+       num_moves = in_check_generator(board_state, moves);
+   } else {
+       num_moves = psuedo_generator(board_state, moves);
+   }
 
    for(int i=0; i<num_moves; ++i) {
        BMove m = moves[i];
@@ -18,10 +23,10 @@ int perft(int depth, BoardState board_state, Stats &stats)
        Square from = get_from(m);
        Square to = get_to(m);
 
-       Colour us = board_state.get_side_to_move();
        Piece p = board_state.get_piece(from);
 
        bool capture = (board_state.get_piece(to) != None);
+
 
        board_state.make_move(m);
        if(!board_state.in_check(us)) {
@@ -36,12 +41,11 @@ int perft(int depth, BoardState board_state, Stats &stats)
            } 
            if(board_state.in_check(!us)) {
                stats.checks++;
-               /* board_state.print_moves(); */
-               /* board_state.print_squares(); */
            }
        }
        board_state.unmake_move(m);
 
+#if 0
        if(!(copy == board_state)) {
            stats.bad_nodes++;
            if(capture)
@@ -53,6 +57,8 @@ int perft(int depth, BoardState board_state, Stats &stats)
            if(flag == OO || flag == OOO) 
                stats.bad_castles++;
        }
+#endif
+
    }
    return nodes;
 }
@@ -71,6 +77,7 @@ void print_perft(int depth, BoardState board_state)
         curr_stats.ep -= prev_stats.ep;
         curr_stats.captures -= prev_stats.captures;
         curr_stats.castles -= prev_stats.castles;
+        curr_stats.checkmates -= prev_stats.checkmates;
 
         std::cout 
             << "depth: " << d 
@@ -79,8 +86,11 @@ void print_perft(int depth, BoardState board_state)
             << " | EP: " << curr_stats.ep 
             << " | castles: " << curr_stats.castles 
             << " | checks: " << curr_stats.checks 
+            << " | checkmates: " << curr_stats.checkmates 
             << '\n';
     }
+
+#if 0
     std::cout
     << " ------------------------------------------------------------------------------ \n"
     << "make_move / unmake_move errors: \n";
@@ -96,6 +106,7 @@ void print_perft(int depth, BoardState board_state)
             << " | castles: " << curr_stats.bad_castles
             << '\n';
     }
+#endif
 
 }
 
--- src/test/perft.h
+++ src/test/perft.h
@@ -12,6 +12,7 @@ struct Stats {
     uint64_t ep = 0;
     uint64_t castles = 0;
     uint64_t checks = 0;
+    uint64_t checkmates = 0;
 
     uint64_t bad_nodes = 0;
     uint64_t bad_captures = 0;
