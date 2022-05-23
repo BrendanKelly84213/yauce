7fe9a5c152b807918626a036f2f7694129d2c880
Brendan Kelly <brewkelly@gmail.com>
2022-05-23 16:36:21 -0700

    search: fixed typo causing always return beta in alphabeta_min

--- search.cpp
+++ search.cpp
@@ -12,46 +12,46 @@ int alphabeta_min(BoardState board, int alpha, int beta, int depth);
 
 int alphabeta_max(BoardState board, int alpha, int beta, int depth)
 {
-	if(depth == 0) return eval(board);
-
     BMove moves[256];
     size_t num_moves = psuedo_generator(board, moves);
     Colour us = board.get_side_to_move();
-    int score;
+
+	if(depth == 0) return eval(board);
 
     for(size_t i = 0; i < num_moves; ++i) {
         BMove m = moves[i];
         board.make_move(m);
-        if( !board.in_check(us) ) 
-            score = alphabeta_min(board, alpha, beta, depth - 1); 
+        if(!board.in_check(us)) {
+            int score = alphabeta_min(board, alpha, beta, depth - 1); 
+            if(score >= beta)
+                return beta; // fail hard
+            if(score > alpha)
+                alpha = score;
+        }
         board.unmake_move(m);
-        if(score >= beta)
-            return beta; // fail hard
-        if(score > alpha)
-            alpha = score;
     }
     return alpha;
 }
 
 int alphabeta_min(BoardState board, int alpha, int beta, int depth)
 {
-	if(depth == 0) return -1 * eval(board);
-
     BMove moves[256];
     size_t num_moves = psuedo_generator(board, moves);
     Colour us = board.get_side_to_move();
-    int score;
+
+	if(depth == 0) return -1 * eval(board);
 
     for(size_t i = 0; i < num_moves; ++i) {
         BMove m = moves[i];
         board.make_move(m);
-        if(!board.in_check(us)) 
-            score = alphabeta_max(board, alpha, beta, depth - 1); 
-        board.make_move(m);
-        if(score <= alpha)
-            return beta; // fail hard
-        if(score < beta)
-            beta = score;
+        if(!board.in_check(us)) {
+            int score = alphabeta_max(board, alpha, beta, depth - 1); 
+            if(score <= alpha)
+                return alpha; // fail hard
+            if(score < beta)
+                beta = score;
+        }
+        board.unmake_move(m);
     }
     return beta;
 }
