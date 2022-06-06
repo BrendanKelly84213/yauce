15ca05d675d606a9168c8f1203e4dd6b43436cf3
Brendan Kelly <brewkelly@gmail.com>
2022-06-05 17:05:24 -0700

    PlayerView: Render pieces given a fen
    This is still rough, but a good starting point for a simple chess GUI

--- GUI/PlayerView.cpp
+++ GUI/PlayerView.cpp
@@ -8,21 +8,7 @@
 // Show available moves on piece selection
 
 #include "PlayerView.h"
-
-enum PieceIndex {
-    WKIdx,
-    WQIdx,
-    WBIdx,
-    WNIdx,
-    WRIdx,
-    WPIdx,
-    BKIdx,
-    BQIdx,
-    BBIdx,
-    BNIdx,
-    BRIdx,
-    BPIdx,
-};
+#include <SDL2/SDL_image.h>
 
 // Init piece clipes relative to piece_sprite_sheet texture
 void init_piece_clips()
@@ -51,9 +37,75 @@ void PlayerView::draw_grid()
     }
 }
 
+// Top left hand corner of square in 
+int PlayerView::square_to_x(Square s) const 
+{
+    size_t f = file(s);
+    return f * square_w;
+}
+
+int PlayerView::square_to_y(Square s) const 
+{
+    size_t r = rank(s);
+    return (7 - r) * square_w;
+}
+
+// BQ, BK, BR, BN, BB, BP, 
+// WQ, WK, WR, WN, WB, WP,None=-1
+
+std::string svg_path[] = {
+    "assets/piece/svg/pixel/bQ.svg",
+    "assets/piece/svg/pixel/bK.svg",
+    "assets/piece/svg/pixel/bR.svg",
+    "assets/piece/svg/pixel/bN.svg",
+    "assets/piece/svg/pixel/bB.svg",
+    "assets/piece/svg/pixel/bP.svg",
+    "assets/piece/svg/pixel/wQ.svg",
+    "assets/piece/svg/pixel/wK.svg",
+    "assets/piece/svg/pixel/wR.svg",
+    "assets/piece/svg/pixel/wN.svg",
+    "assets/piece/svg/pixel/wB.svg",
+    "assets/piece/svg/pixel/wP.svg"
+};
+
+SDL_Texture * PlayerView::piece_texture(Piece p)
+{
+    std::string path = svg_path[p];
+    printf("Got path: %s\n", path.c_str());
+    SDL_Surface * surface = IMG_Load(path.c_str());
+    if(surface == NULL) {
+        printf("Error getting surface from svg: %s, %s\n", path.c_str(), SDL_GetError());
+        exit(1);
+        return NULL;
+    }
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(board_renderer, surface);
+    SDL_FreeSurface(surface);
+    if(texture == NULL) {
+        printf("Error creating texture from surface: %s\n", SDL_GetError());
+        exit(1);
+        return NULL;
+    }
+    return texture;
+}
+
+void PlayerView::draw_piece(Piece p, Square s)
+{
+    int x = square_to_x(s);
+    int y = square_to_y(s);
+
+    SDL_Texture * texture = piece_graphics[p];
+
+    SDL_Rect rect = { x, y, square_w, square_w };
+    SDL_RenderCopy(board_renderer, texture, NULL, &rect);
+}
+
 void PlayerView::draw_pieces()
 { 
-    /* SDL_RenderCopy(board_renderer, piece_sprite_sheet, &piece_clip, &dest_rect); */
+    for(Square s = a1; s <= h8; ++s) {
+        Piece p = squares[s];
+        if(p != None)
+            draw_piece(p, s);
+    }
 }
 
 void PlayerView::on_player_move_piece()
@@ -80,6 +132,7 @@ void PlayerView::handle_events(SDL_Event e)
         running = false;
 }
 
+
 bool PlayerView::init(std::string fen)
 { 
     SDL_GetWindowSize(board_window, &window_w, &window_h);
@@ -91,14 +144,34 @@ bool PlayerView::init(std::string fen)
     if(SDL_CreateWindowAndRenderer(640, 640, SDL_WINDOW_RESIZABLE, &board_window, &board_renderer)) {
         return false;
     }
+
+    // Init image
+    IMG_Init(IMG_INIT_PNG);
+
+    // Init position
+    init_squares(fen);
+    init_pieces();
      
     return true;
 } 
+
+void PlayerView::init_pieces()
+{
+    for(Square s = a1; s <= h8; ++s) {
+        Piece p = squares[s];
+        if(p != None) {
+            piece_graphics[p] =  piece_texture(p);
+        }
+    }
+}
 
 void PlayerView::run()
 {
     running = true;
     while(running) {
+
+        uint64_t start = SDL_GetPerformanceCounter();
+
         // Handle events 
         if(SDL_PollEvent(&e)) {
             handle_events(e);
@@ -107,9 +180,16 @@ void PlayerView::run()
         SDL_SetRenderDrawColor(board_renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(board_renderer);
         draw_grid();
-        /* draw_pieces(); */
+        draw_pieces(); 
         /* draw_available_moves(); */
         SDL_RenderPresent(board_renderer);
+
+        uint64_t end = SDL_GetPerformanceCounter();
+
+        float elapsed = (end - start) / (float)(SDL_GetPerformanceFrequency() * 1000.0f);
+        
+        // Cap frames
+        SDL_Delay(floor(16.66f - elapsed));
     }
 
     SDL_DestroyRenderer(board_renderer);
@@ -119,3 +199,162 @@ void PlayerView::run()
     SDL_Quit(); 
 }
 
+// Following section borrowed from BoardState.h
+// In the future perhaps move to shared utility file, includes, something
+
+enum Section 
+{ 
+    Board, 
+    SideToMove, 
+    CastlingRights, 
+    EPSquare, 
+    HalfmoveClock, 
+    FullmoveCounter 
+};
+
+static bool is_piece_ch(char ch) 
+{
+    return (
+            ch == 'p'
+         || ch == 'r'
+         || ch == 'q'
+         || ch == 'k'
+         || ch == 'n'
+         || ch == 'b'
+         || ch == 'P'
+         || ch == 'R'
+         || ch == 'Q'
+         || ch == 'K'
+         || ch == 'N'
+         || ch == 'B'
+    );
+}
+
+static Piece fen_to_piece(char ch) 
+{
+    switch(ch) {
+        case 'p': 
+            return BP;
+        case 'r':
+            return BR;
+        case 'q':
+            return BQ;
+        case 'k':
+            return BK;
+        case 'n':
+            return BN;
+        case 'b':
+            return BB;
+        case 'P':
+            return WP;
+        case 'R':    
+            return WR;
+        case 'Q':    
+            return WQ;
+        case 'K':    
+            return WK;
+        case 'N':    
+            return WN;
+        case 'B':    
+            return WB;
+        default: break;
+    }
+    return None;
+}
+
+constexpr Square square(int rank, int file)
+{
+    return (Square)(rank * 8 + file);
+}
+
+void PlayerView::init_squares(std::string fen)
+{
+    // Parse out pieces 
+    int rank=7;
+    int file=0;
+    int i=0;
+    int section=0;
+    
+    for(int i=a1; i<=h8; ++i) {
+        squares[i] = None;
+    }
+
+    while(fen[i] != ' ') {
+        int sq = (rank)*8 + file;
+
+        if(fen[i] == '/') {
+            rank--;
+            file=0;
+        }
+
+        if(fen[i] >= '0' && fen[i] <= '8') {
+            file += (fen[i] - 0x30);
+        }
+
+        if(is_piece_ch(fen[i])) {
+            squares[sq] = fen_to_piece(fen[i]);
+            file++;
+        } 
+
+        i++;
+    }
+
+    section++;
+
+    // Parse out info
+    std::string info = fen.substr(i + 1, fen.length());
+    for(int i=0; i<info.length(); ++i) {
+        if(info[i] == ' ') {
+            section++;
+            /* continue; */
+        }
+        switch(section) {
+            case SideToMove: 
+                if(info[i] == 'w') {
+                    state.side_to_move = White;
+                } else {
+                    state.side_to_move = Black;
+                }
+                break;
+            case CastlingRights:
+                switch(info[i]) {
+                    case '-':
+                        state.w_castle_ks = false; 
+                        state.w_castle_qs = false; 
+                        state.b_castle_ks = false; 
+                        state.b_castle_qs = false; 
+                        break;
+                    case 'k':
+                        state.b_castle_ks = true;
+                        break;
+                    case 'q':
+                        state.b_castle_qs = true;
+                        break;
+                    case 'K': 
+                        state.w_castle_ks = true;
+                        break;
+                    case 'Q':
+                        state.w_castle_qs = true;
+                        break;
+                    default: break;
+                }
+                break;
+            case EPSquare:
+                if(info[i] == '-') {
+                    state.ep_square = NullSquare;
+                } else if(info[i] >= 'a' && info[i] <= 'h') {
+                    int ep_file = static_cast<int>(info[i] - 0x61);
+                    int ep_rank = state.side_to_move == White ? 4 : 5;
+                    state.ep_square = square(ep_rank, ep_file);
+                } 
+                break;
+            case HalfmoveClock:
+                state.halfmove_clock = static_cast<int>(info[i] - 0x30);
+                break;
+            case FullmoveCounter:
+                state.ply_count = static_cast<int>(info[i] - 0x30);
+                break;
+            default: break;
+        }
+    }
+}
--- GUI/PlayerView.h
+++ GUI/PlayerView.h
@@ -2,14 +2,55 @@
 #define PLAYERVIEW_H
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include <string>
+
+// For now borrow these structs / enums from BoardState 
+
+enum Piece : int { 
+    BQ, BK, BR, BN, BB, BP, 
+    WQ, WK, WR, WN, WB, WP,None=-1
+};
+
+enum Square : int {
+	a1, b1, c1, d1, e1, f1, g1, h1,
+	a2, b2, c2, d2, e2, f2, g2, h2, 
+	a3, b3, c3, d3, e3, f3, g3, h3,
+	a4, b4, c4, d4, e4, f4, g4, h4, 
+	a5, b5, c5, d5, e5, f5, g5, h5,
+	a6, b6, c6, d6, e6, f6, g6, h6,
+	a7, b7, c7, d7, e7, f7, g7, h7,
+	a8, b8, c8, d8, e8, f8, g8, h8, NullSquare = -1
+};
+
+constexpr void operator++(Square &s) 
+{
+    s = (Square)((int)s + 1);
+}
+
+enum Colour { White, Black };
+
+struct State {
+    Colour side_to_move = White; 
+    // TODO: Castle can be a lot more lightweight...
+    bool w_castle_ks = false; 
+    bool w_castle_qs = false; 
+    bool b_castle_ks = false; 
+    bool b_castle_qs = false; 
+    Square ep_square = NullSquare;
+    // FIXME: int -> size_t
+    int halfmove_clock = 0;
+    int ply_count = 0;     
+    Piece last_captured = None;
+
+    bool operator==(State &b) const;
+};
 
 class PlayerView {
 private: 
 
     bool running = false;
+
+    size_t tick_count = 0;
     
     SDL_Event e;
     SDL_Window* board_window = NULL;
@@ -19,17 +60,32 @@ private:
     int window_h;
     int square_w;
 
+    Piece squares[64];
+    SDL_Texture * piece_graphics[12];
+
+    State state;
+
     void draw_grid();
+    void draw_piece(Piece p, Square s);
     void draw_pieces();
     void on_player_move_piece();
     void draw_available_moves();
     void handle_events(SDL_Event e);
     int xy_to_square(int x, int y);
+    void init_squares(std::string fen);
+    void init_pieces();
+
+    size_t rank(Square s) const { return s >> 3; };
+    size_t file(Square s) const {return s % 8; }
+
+    int square_to_x(Square s) const;
+    int square_to_y(Square s) const;
+
+    SDL_Texture * piece_texture(Piece p);
 
 public:
     bool init(std::string fen);
     void run();
 };
-
 
 #endif
