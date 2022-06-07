0ba72b944c2537b886eb76c275de4a24350d0d70
Brendan Kelly <brewkelly@gmail.com>
2022-06-06 19:38:33 -0700

    GUI: move square(rank, file) helper to helpers.h

--- GUI/helpers.h
+++ GUI/helpers.h
@@ -19,4 +19,12 @@ constexpr int square_to_y(Square s, int square_w)
     return (7 - r) * square_w;
 }
 
+constexpr size_t x_to_file(int x, int square_w) { return floor(x / square_w); }
+constexpr size_t y_to_rank(int y, int square_w) { return 7 - floor(y / square_w); }
+
+constexpr Square square(size_t rank, size_t file)
+{
+    return (Square)(rank * 8 + file);
+}
+
 #endif
