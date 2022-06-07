5569ed05d06d708c32ae5934159fbcc9a5584d1b
Brendan Kelly <brewkelly@gmail.com>
2022-06-06 19:05:13 -0700

    GUI: Add BoardPiece class for handling piece textures and positions

---
+++ GUI/BoardPiece.cpp
@@ -0,0 +1,52 @@
+#include "BoardPiece.h"
+#include "helpers.h"
+#include <SDL2/SDL_image.h>
+
+
+void BoardPiece::update(int x, int y, int w)
+{
+    rect.w = w; 
+    rect.h = w; 
+    rect.x = x;
+    rect.y = y;
+}
+
+void BoardPiece::update(int w)
+{
+    int x = square_to_x(s, w);
+    int y = square_to_y(s, w);
+    rect.w = w;
+    rect.h = w;
+    rect.x = x;
+    rect.y = y;
+}
+
+void BoardPiece::init(Piece _p, Square _s, int w, SDL_Renderer * renderer)
+{
+    int x = square_to_x(s, w);
+    int y = square_to_y(s, w);
+    p = _p;
+    s = _s;
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = w;
+    make_piece_texture(renderer);
+}
+
+void BoardPiece::make_piece_texture(SDL_Renderer * renderer)
+{
+    std::string path = svg_path[p];
+    printf("Got path: %s\n", path.c_str());
+    SDL_Surface * surface = IMG_Load(path.c_str());
+    if(surface == NULL) {
+        printf("Error getting surface from svg: %s, %s\n", path.c_str(), SDL_GetError());
+        exit(1);
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if(texture == NULL) {
+        printf("Error creating texture from surface: %s\n", SDL_GetError());
+        exit(1);
+    }
+}
---
+++ GUI/BoardPiece.h
@@ -0,0 +1,54 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <string>
+#include "../utils/types.h"
+
+const std::string svg_path[] = {
+    "assets/piece/png/cburnett/bQ.png",
+    "assets/piece/png/cburnett/bK.png",
+    "assets/piece/png/cburnett/bR.png",
+    "assets/piece/png/cburnett/bN.png",
+    "assets/piece/png/cburnett/bB.png",
+    "assets/piece/png/cburnett/bP.png",
+    "assets/piece/png/cburnett/wQ.png",
+    "assets/piece/png/cburnett/wK.png",
+    "assets/piece/png/cburnett/wR.png",
+    "assets/piece/png/cburnett/wN.png",
+    "assets/piece/png/cburnett/wB.png",
+    "assets/piece/png/cburnett/wP.png"
+};
+
+class BoardPiece {
+public:
+
+    BoardPiece() : dragging(false), p(WP), s(a1), texture(NULL), rect({0,0,0,0}) 
+    {
+    }
+
+    void init(Piece _p, Square _s, int w, SDL_Renderer * renderer);
+    void update(int x, int y, int w);
+    void update(int w);
+
+    bool is_being_dragged() const { return dragging; }
+    bool in_piece(int x, int y) const { return x > rect.x && x < (rect.x + rect.w) && y > rect.y && y < (rect.y + rect.w); }
+    Piece get_piece() const { return p; } 
+    Square get_square() const { return s; }
+    SDL_Texture * get_texture() const { return texture; }
+    SDL_Rect get_rect() const { return rect; }
+
+    void toggle_dragging() { dragging = !dragging; }
+    void set_dragging(bool _dragging) { dragging = _dragging; }
+
+private:
+
+    bool dragging;
+    Piece p;
+    Square s;
+    SDL_Texture * texture;
+    SDL_Rect rect;
+
+    void make_piece_texture(SDL_Renderer * renderer);
+};
+
+#endif
