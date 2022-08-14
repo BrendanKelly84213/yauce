be8287c7c50ae4157c42a5055f6fc62c485a13c8
Brendan Kelly <brewkelly@gmail.com>
2022-08-14 12:12:07 -0400

    add getters for size and max_size to transposition class

--- transposition.h
+++ transposition.h
@@ -11,7 +11,7 @@ struct TTItem {
     size_t depth;
     BMove move;
 
-    TTItem() : key(0), type(EXACT), score(0) {}
+    // TTItem() : key(0), type(EXACT), score(0) {}
     TTItem(ZobristKey k, NodeType t, int s, size_t d, BMove m) 
         : key(k), type(t), score(s), depth(d), move(m) {}
 };
@@ -26,7 +26,8 @@ public:
    
     void insert(ZobristKey key, NodeType type, int score, size_t depth, BMove m);
     const TTItem* get(ZobristKey hash) const;
-    size_t num_items { 0 };
+    size_t size() const { return buckets.size(); }
+    size_t max_size() const { return buckets.max_size(); }
 
     void reset();
 private:
